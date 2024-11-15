39ab4f6638bdd27cfd09b3d086779bbe8a05c911
jczars <jczars@gmail.com>
2024-11-15 18:03:09 -0300

    del folders update

--- models/del_folders_limiar.py
+++ models/del_folders_limiar.py
@@ -19,7 +19,7 @@ def del_folder(path, flag=0):
 # In[2]:
 def del_vistas(params):
     for vt in params['vistas']:
-        path_vistas=params['bd_src']+'/'+vt
+        path_vistas=params['bd_dst']+'/'+vt
         #print(path_vistas)
         cat_names = sorted(os.listdir(path_vistas))
         
